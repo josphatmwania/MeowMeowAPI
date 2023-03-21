196233b8cc3f7173874c1d0e1015c4eead131735
josphatmwania <josphatmwania237@gmail.com>
2023-03-21 23:29:37 +0300

    Add API get Method

--- app/src/main/java/com/josphat/meowmeowapi/api/ApiService.kt
+++ app/src/main/java/com/josphat/meowmeowapi/api/ApiService.kt
@@ -1,4 +1,10 @@
 package com.josphat.meowmeowapi.api
 
+import com.josphat.meowmeowapi.data.CatImageResponse
+import okhttp3.Response
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("api/https://api.thecatapi.com/v1/images/search?limit=1")
+    suspend fun getRandomCatImage() : Response<CatImageResponse>
 }
