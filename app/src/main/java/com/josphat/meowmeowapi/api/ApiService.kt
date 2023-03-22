02cddab3dd28c8919ea2b168615e63c71109d755
josphatmwania <josphatmwania237@gmail.com>
2023-03-22 17:01:00 +0300

    Edit API service

--- app/src/main/java/com/josphat/meowmeowapi/api/ApiService.kt
+++ app/src/main/java/com/josphat/meowmeowapi/api/ApiService.kt
@@ -2,7 +2,7 @@ package com.josphat.meowmeowapi.api
 
 import com.josphat.meowmeowapi.data.CatImageResponse
 import okhttp3.Response
-import retrofit2.http.GET
+import retrofit.http.GET
 
 interface ApiService {
     @GET("api/https://api.thecatapi.com/v1/images/search?limit=1")
