b3216d696152490f8638995060e81bf7ae07a23d
josphatmwania <josphatmwania237@gmail.com>
2023-04-24 14:00:15 +0300

    Add BaseUrl and end points

--- app/src/main/java/com/josphat/meowmeowapi/data/api/ApiConstants.kt
+++ app/src/main/java/com/josphat/meowmeowapi/data/api/ApiConstants.kt
@@ -2,5 +2,5 @@ package com.josphat.meowmeowapi.data.api
 
 object ApiConstants {
     const val BASE_URL = "https://api.thecatapi.com/"
-    const val END_POINTS = "https://api.thecatapi.com/v1/images/search"
+    const val END_POINTS = "images"
 }
