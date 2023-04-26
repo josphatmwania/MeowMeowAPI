15f0fecfe11f05685d091ed5a657b8ea9c67583d
josphatmwania <josphatmwania237@gmail.com>
2023-04-26 09:28:36 +0300

    Add di

--- app/src/main/java/com/josphat/meowmeowapi/di/CatApiModule.kt
+++ app/src/main/java/com/josphat/meowmeowapi/di/CatApiModule.kt
@@ -1,11 +1,27 @@
 package com.josphat.meowmeowapi.di
 
+import com.josphat.meowmeowapi.data.api.CatApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
-class CatApiModule {
+object CatApiModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(builder: Retrofit.Builder): CatApi{
+        return builder
+            .build()
+            .create(CatApi::class.java)
+
+    }
+
+
+
 }
