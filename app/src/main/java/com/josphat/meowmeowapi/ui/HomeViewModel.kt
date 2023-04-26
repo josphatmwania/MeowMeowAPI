bf74b7ec14f53b3e17c346b7c4bed8ea21a3232e
josphatmwania <josphatmwania237@gmail.com>
2023-04-26 16:37:58 +0300

    Implement the Viewmodel

--- app/src/main/java/com/josphat/meowmeowapi/ui/HomeViewModel.kt
+++ app/src/main/java/com/josphat/meowmeowapi/ui/HomeViewModel.kt
@@ -1,4 +1,14 @@
 package com.josphat.meowmeowapi.ui
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.josphat.meowmeowapi.data.repository.CatRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val catRepo: CatRepo
+) : ViewModel() {
+
 }
