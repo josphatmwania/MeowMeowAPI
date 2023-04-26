d894036462295a3ba80f2960880fafd2d74afc79
josphatmwania <josphatmwania237@gmail.com>
2023-04-26 17:19:45 +0300

    Implement the Viewmodel

--- app/src/main/java/com/josphat/meowmeowapi/ui/HomeViewModel.kt
+++ app/src/main/java/com/josphat/meowmeowapi/ui/HomeViewModel.kt
@@ -1,8 +1,13 @@
 package com.josphat.meowmeowapi.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.josphat.meowmeowapi.data.api.model.Cat
 import com.josphat.meowmeowapi.data.repository.CatRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
@@ -10,5 +15,17 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val catRepo: CatRepo
 ) : ViewModel() {
+
+    private val _state = MutableStateFlow(emptyList<Cat>())
+    val state: StateFlow<List<Cat>>
+        get() = _state
+
+
+    init {
+        viewModelScope.launch {
+            val cat = catRepo.getCat()
+            _state.value = cat
+        }
+    }
 
 }
