1f9eec64d307570e8ac46e0826951db6b1c3ac0a
josphatmwania <josphatmwania237@gmail.com>
2023-04-26 09:33:41 +0300

    Add di for Retrofit

--- app/src/main/java/com/josphat/meowmeowapi/di/CatApiModule.kt
+++ app/src/main/java/com/josphat/meowmeowapi/di/CatApiModule.kt
@@ -1,11 +1,13 @@
 package com.josphat.meowmeowapi.di
 
+import com.josphat.meowmeowapi.data.api.ApiConstants
 import com.josphat.meowmeowapi.data.api.CatApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 
@@ -20,6 +22,14 @@ object CatApiModule {
             .build()
             .create(CatApi::class.java)
 
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit() : Retrofit.Builder{
+        return Retrofit.Builder()
+            .baseUrl(ApiConstants.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
     }
 
 
